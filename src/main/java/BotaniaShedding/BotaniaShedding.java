f901c48f152415e9c2d6f2671182aa14166790cc
Ehwuts Matsu <tsukan.git@gmail.com>
2016-11-21 01:32:44 -0800

    make sure botania is absent before doing things

--- src/main/java/BotaniaShedding/BotaniaShedding.java
+++ src/main/java/BotaniaShedding/BotaniaShedding.java
@@ -12,9 +12,12 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "botaniashedding", name="BotaniaShedding", version = "1.0", dependencies = "after:Botania")
+@Mod(modid = "botaniashedding", name="BotaniaShedding", version = "1.1", dependencies = "after:Botania")
 public class BotaniaShedding
-{	
+{
+	//have this mod neuter itself on the off chance they get loaded together
+	public boolean botaniaLoaded;
+	
 	public static Configuration config;
 
 	public void saveConfig() {
@@ -24,6 +27,9 @@ public class BotaniaShedding
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		botaniaLoaded = Loader.isModLoaded("Botania");
+		if (botaniaLoaded) return;
+		
 		config = new Configuration(event.getSuggestedConfigurationFile());
 
 		config.load();
@@ -32,11 +38,15 @@ public class BotaniaShedding
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
+		if (botaniaLoaded) return;
+		
 		MinecraftForge.EVENT_BUS.register(new SheddingHandler());
 	}
-	@EventHandler
 	
+	@EventHandler	
 	public void postInit(FMLPostInitializationEvent event) {
+		if (botaniaLoaded) return;
+		
 		SheddingHandler.loadFromConfig(config);
 		saveConfig();
 	}
